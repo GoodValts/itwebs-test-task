589a10498d0d88ac599b0a01ebc4cdb1752647ff
goodvalts <martiniukgomel@gmail.com>
2025-09-30 15:43:52 +0300

    fix: add window reload to fetching error

--- src/components/fetching-error/fetching-error.tsx
+++ src/components/fetching-error/fetching-error.tsx
@@ -1,10 +1,12 @@
 import { Button } from '../ui/button/button';
 import styles from './fetching-error.module.scss';
 
-export const FetchingError = ({ code, message }: { code: number; message: string }) => (
+export const FetchingError = ({ code, message }: { code: number; message?: string }) => (
 	<div className={styles.container}>
 		<h2 className={styles.header}>Error {code}</h2>
 		{message && <p className={styles.text}>{message}</p>}
-		<Button variant="destructive">Reload</Button>
+		<Button variant="destructive" onClick={() => window.location.reload()}>
+			Reload
+		</Button>
 	</div>
 );
